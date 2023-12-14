3f78e82c6203fc9634e713312237093145909753
bouzagui <badrbouzagui@gmail.com>
2023-12-14 17:40:53 +0100

    Print string by write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main - entry point
  * Return: Always 1.
  */
- int main(void)
- {
+int main(void)
+{
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str, 59);
- }
+}
