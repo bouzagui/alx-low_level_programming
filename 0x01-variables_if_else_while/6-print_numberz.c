9b9c9f661156f8aae2fc24ce95b77ca950f00fe0
bouzagui <badrbouzagui@gmail.com>
2023-12-15 16:05:48 +0000

    printed numbers with ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Description: function to be printed numbers with ASCII.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+        i++;
+	}
+	putchar('\n');
+	return (0);
+}
