c315a5a9b4372ecbd319ec7f6d9afed6c1fe936b
bouzagui <badrbouzagui@gmail.com>
2023-12-19 16:21:43 +0100

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,18 @@
 /**
  * main - Entry point.
  * description: function to be printed _putchar.
- * 
+ *
  * Return: Always 0
 */
 int main(void)
 {
-    size_t i;
-    char str[] = "_putchar";
+	size_t i;
+	char str[] = "_putchar";
 
-    for (i = 0; i < strlen(str); i++)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
-    return (0);
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
