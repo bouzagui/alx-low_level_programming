28e3c142f1b4ed0ad892bc3f5695fb5c64952896
bouzagui <badrbouzagui@gmail.com>
2023-12-19 17:34:56 +0100

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to be printed alphabet ten more.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int alp;
+	int more;
+
+	for (more = 0; more < 10; more++)
+	{
+		for (alp = 97; alp <= 122; alp++)
+		{
+			_putchar(alp);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
