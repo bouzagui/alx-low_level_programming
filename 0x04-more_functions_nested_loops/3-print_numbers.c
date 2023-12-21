cfcc02c64e6dcb1fb45e7c3164c6fca44ddab4bd
bouzagui <badrbouzagui@gmail.com>
2023-12-21 11:54:32 +0000

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: Always 0.
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
