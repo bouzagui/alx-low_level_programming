777c17e1d481837ea01d6ff3daaf898112875af2
bouzagui <badrbouzagui@gmail.com>
2023-12-21 12:30:15 +0000

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,13 @@
  * Return: nothing.
 */
 void print_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 47; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
 	}
+	_putchar('\n');
 }
