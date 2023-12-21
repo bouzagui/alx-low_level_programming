4e74b1b39c59264271965ec79dc1aef8677c5d71
bouzagui <badrbouzagui@gmail.com>
2023-12-21 11:15:04 +0000

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,17 +4,17 @@
 /**
  * _isupper - function to be checked for upper case characters.
  * @c: The argument to be checked for upper case characters.
- * 
+ *
  * Return: if the argument is uppercase return 1, or 0 otherwise.
 */
 int _isupper(int c)
 {
-    if (isupper(c))
-    {
-        return (1);
-    }
-    else if (islower(c))
-    {
-        return (0);
-    }
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
