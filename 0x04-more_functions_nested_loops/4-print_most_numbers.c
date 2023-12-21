e0a3934ada31ea84e66dfff60705e0d793941a3c
bouzagui <badrbouzagui@gmail.com>
2023-12-21 16:06:31 +0000

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print the most numbers.
+ *
+ * Return: Nothing. 
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for(i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	putchar('\n');
+}
