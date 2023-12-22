b585f51089d51e584b4dd04bfbd68a684b856bad
bouzagui <badrbouzagui@gmail.com>
2023-12-22 00:22:47 +0000

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - function to be printed character '_'.
+ * @n: number of characters to print
+ *
+ * Return: number of characters printed.
+*/
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (n > i)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
