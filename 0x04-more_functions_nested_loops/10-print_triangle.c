4f8fe820cd124d9e7639193fceaf9bcb19bd8d10
bouzagui <badrbouzagui@gmail.com>
2023-12-22 02:58:02 +0000

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - entry point
+ * Description: prints a triangle
+ * @size: size of the triangle
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int hash, index;
+
+	if (size > 0)
+	{
+		for (hash = 1; hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+				_putchar(' ');
+
+			for (index = 0; index < hash; index++)
+				_putchar('#');
+
+			if (hash == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
