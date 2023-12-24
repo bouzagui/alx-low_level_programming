af58efea395c5c5ab90be929816ea836eac966b9
bouzagui <badrbouzagui@gmail.com>
2023-12-24 01:09:50 +0000

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,24 +2,24 @@
 
 /**
  * jack_bauer - Function to be printed time.
- * 
+ *
  * Return: Nothing.
 */
 void jack_bauer(void)
 {
-    int hours = 0, mins;
+	int hours = 0, mins;
 
-    while (hours < 24)
-    {
-        for (mins = 0; mins < 60; mins++)
-        {
-            _putchar((hours / 10) + '0');
-            _putchar((hours % 10) + '0');
-            _putchar(':');
-            _putchar((mins / 10) + '0');
-            _putchar((mins % 10) + '0');
-            _putchar('\n');
-        }
-        hours++;
-    }
+	while (hours < 24)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+		hours++;
+	}
 }
