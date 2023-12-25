9949afe75877550ccd066176c05956d742f71822
bouzagui <badrbouzagui@gmail.com>
2023-12-25 01:27:26 +0000

    done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print all solutions numbers to 98 if less or than.
+ * @n: Number of solutions.
+ * 
+ * Return: Numbers of solutions
+*/
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+}
