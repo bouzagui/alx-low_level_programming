94cfd2aba35e398492a06c8afd3aa69e1f05c428
bouzagui <badrbouzagui@gmail.com>
2023-12-29 15:32:12 +0000

    done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,14 +2,21 @@
 
 int main(void)
 {
-    int i, a = 0, b = 1, sum = 0;
+    long i, a = 0, b = 1, sum = 0;
 
     for (i = 0; i < 50; i++)
     {
             sum = a + b;
             a = b;
             b = sum;
-            printf("%d, ", b);
+            printf("%lu", b);
+            if (b == 49)
+            {
+                printf("\n");
+            }
+            else
+            {
+                printf(", ");
+            }
     }
-    printf("\n");
 }
