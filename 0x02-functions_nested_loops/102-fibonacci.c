cc0e53c8da49f10d410629af15e80a19b1dcbe03
bouzagui <badrbouzagui@gmail.com>
2023-12-29 15:52:09 +0000

    done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the Fibonacci of the first 50.
+ *
+ * Return: Always 0 on success.
+*/
 int main(void)
 {
-    long i, a = 0, b = 1, sum = 0;
+	long i, a = 0, b = 1, sum = 0;
 
-    for (i = 0; i < 50; i++)
-    {
-            sum = a + b;
-            a = b;
-            b = sum;
-            printf("%lu", b);
-            if (b == 49)
-                printf("\n");
-            else
-                printf(", ");
-    }
-    printf("\n");
-    return (0);
+	for (i = 0; i < 50; i++)
+	{
+			sum = a + b;
+			a = b;
+			b = sum;
+
+			printf("%lu", b);
+			if (i == 49)
+			{
+				printf("\n");
+			}
+			else
+			{
+				printf(", ");
+			}
+	}
+	return (0);
 }
