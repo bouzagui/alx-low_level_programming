d05b74c575381deb38352c589b37937ec9a8efc2
bouzagui <badrbouzagui@gmail.com>
2023-12-29 15:14:11 +0000

    done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, a = 0, b = 1, sum = 0;
+
+    for (i = 0; i <= 50; i++)
+    {
+            sum = a + b;
+            a = b;
+            b = sum;
+            printf("%d, ", b);
+    }
+    printf("\n");
+}
