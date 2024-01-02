fd49273b8e8d85303be03254c5e7ed619e6e3443
bouzagui <badrbouzagui@gmail.com>
2024-01-02 22:54:06 +0000

    done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * swap_int - Function to be printed swaps the values
+ * @a: First value.
+ * @b: Second value.
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
