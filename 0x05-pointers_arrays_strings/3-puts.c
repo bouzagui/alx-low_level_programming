49ec11c291711fe1824c9e89913d4e6dcdadd23f
bouzagui <badrbouzagui@gmail.com>
2024-01-03 01:21:57 +0000

    done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 #include <string.h>
 
 void _puts(char *str)
 {
+    int i;
+    size_t length = strlen(str);
 
-    while (*str <= '\0')
+    for (i = 0; i < length; i++)
     {
-        str++;
-        printf("%s\n", str);
+        _putchar(str[i]);
     }
+    _putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
