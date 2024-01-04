4f566b758136ad37cfa7a9a6753297f3f55d4676
bouzagui <badrbouzagui@gmail.com>
2024-01-04 14:17:54 +0000

    done

---
+++ 0x06-pointers_arrays_string/0-strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strcat - Function to be printed string interconnection
+ * @dest: destination.
+ * @src: source string.
+ * Return: destination string.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, count = 0;
+
+	for (i = 0; src[i]; i++)
+	{
+	}
+	while (src[count])
+	{
+		count++;
+	}
+	return (dest);
+}
