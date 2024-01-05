416cc1134dbcf8e25d3eff3f060903ea34f5f765
bouzagui <badrbouzagui@gmail.com>
2024-01-05 00:43:59 +0000

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,22 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int o = 0;
-	int i = 0;
+	int o;
+	int i;
 
+	o = 0;
 	while (dest[o] != '\0')
 	{
 		o++;
 	}
 
+	i = 0;
 	while (src[i] != '\0')
 	{
-		dest[o] = src[i];
+		*(dest + i) = *(src + i);
 		o++;
 		i++;
 	}
 	dest[o] = '\0';
-
 	return (dest);
 }
