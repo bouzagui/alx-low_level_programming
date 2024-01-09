a6be7b718f611353b0e4420c51865583a941aed9
bouzagui <badrbouzagui@gmail.com>
2024-01-09 01:04:11 +0000

    done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - fills memory with a constant byte.
+ * @s: first bytes of the memory
+ * @c: constant byte b
+ * Return: Null when not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] != c)
+		{
+			s++;
+		}
+		else
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
