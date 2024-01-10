2bbb8c88dbe594ce2bb0416843706597c4f6acc2
bouzagui <badrbouzagui@gmail.com>
2024-01-10 00:11:35 +0000

    done

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,11 +8,10 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j;
+	int i = 0, j = 0;
 
 	while (s[i] != '\0')
 	{
-		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
