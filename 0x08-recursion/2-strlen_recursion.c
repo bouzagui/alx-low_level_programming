b48514f92123f8fd0e10dde0a8f9a4b4d33ba5d5
bouzagui <badrbouzagui@gmail.com>
2024-01-10 18:33:09 +0000

    done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -17,6 +16,6 @@ int _strlen_recursion(char *s)
 	{
 		len = _strlen_recursion(s + 1);
 		len++;
-		return (len);
 	}
+    return (len);
 }
