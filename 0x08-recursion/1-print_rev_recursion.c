9d1bac04a8379c20c8aab1b74f44ec8f9c04e646
bouzagui <badrbouzagui@gmail.com>
2024-01-10 17:59:23 +0000

    done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - print a string in reverse.
+ * @s: the string to print
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		printf("%c", *s);
+	}
+}
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
