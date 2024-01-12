d92330d25ede86630163ab07c5cd0b8c255673db
bouzagui <badrbouzagui@gmail.com>
2024-01-12 13:03:40 +0000

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Enter point.
- * @argc: Number of arguments
- * @argv: Array of arguments.
- * Retrun: Always 0 (success)
-*/
+ * @argc: Number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char **argv)
 {
 	if (argc >= 0)
