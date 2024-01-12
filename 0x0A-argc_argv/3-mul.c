9628e851f94bfb4cffed5ecf73dc63f8c7c5e82b
bouzagui <badrbouzagui@gmail.com>
2024-01-12 14:11:19 +0000

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Enter point.
+ *
+ * @argc: Number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error");
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	return (0);
+}
