7b0c2681dbacfe1a13ed26f130425fe52799c8f9
bouzagui <badrbouzagui@gmail.com>
2024-01-12 13:33:03 +0000

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: number of arguments
+ * @argv: array of arguments.
+ *
+ * Return: Always 0.
+*/
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+    
+	return (0);
+}
