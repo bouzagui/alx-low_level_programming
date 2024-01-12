95cfb59a5a9fa02d66053198db2bf22580f4ebdc
bouzagui <badrbouzagui@gmail.com>
2024-01-12 14:20:20 +0000

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,18 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2;
+    int num1, num2;
 
-	if (argc != 3)
-	{
-		printf("Error");
-		return (1);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	printf("%d\n", num1 * num2);
-	return (0);
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    else
+    {
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+        printf("%d\n", num1 * num2);
+    }
+    return (0);
 }
