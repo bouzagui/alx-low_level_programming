35a5e055446901144154447711cf52594d2d307e
bouzagui <badrbouzagui@gmail.com>
2024-01-15 18:00:05 +0000

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,21 +8,21 @@
 */
 char *_strdup(char *str)
 {
-    char *ptr;
-    size_t length;
+	char *ptr;
+	size_t length;
 
-    if (str == 0)
-    {
-        return (NULL);
-    }
-    length = strlen(str);
-    ptr = malloc((length + 1) * sizeof(char));
-    {
-        if (ptr == NULL)
-        {
-            return (NULL);
-        }
-    }
-    strcpy(ptr, str);
-    return (ptr);
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	length = strlen(str);
+	ptr = malloc((length + 1) * sizeof(char));
+	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+	}
+	strcpy(ptr, str);
+	return (ptr);
 }
