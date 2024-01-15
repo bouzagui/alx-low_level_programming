5f7c9a782891b4355247d2c4a37aaa3cefb2d1ab
bouzagui <badrbouzagui@gmail.com>
2024-01-15 14:07:05 +0000

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,25 +1,28 @@
 #include "main.h"
-
+/**
+ * create_array - create an array from a list of integers.
+ * @size: size of the array
+ * @c: character array.
+ * Return: the new array.
+*/
 char *create_array(unsigned int size, char c)
 {
-    char *ptr;
-    unsigned int i = 0;
+	char *ptr;
+	unsigned int i = 0;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    ptr = malloc(size * sizeof(char));
-    if (ptr == NULL)
-    {
-        exit(0);
-    }
-    while (i < size)
-    {
-        ptr[i] = c;
-        i++;
-    }
-    return (ptr);
-
-    
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+	{
+		exit(0);
+	}
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
 }
