62b9a0fdc085e29ddf96d02dd832afb24a6827d1
bouzagui <badrbouzagui@gmail.com>
2024-01-15 18:25:57 +0000

    done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+char *str_concat(char *s1, char *s2)
+{
+    char *ptr;
+    size_t str1 = strlen(s1);
+    size_t str2 = strlen(s2);
+
+    ptr = malloc((str1 + str2 + 1) * sizeof(char));
+    if (ptr == NULL)
+    {
+        exit(0);
+    }
+    strcpy(ptr, s1);
+    strcpy(ptr, s2);
+    return (ptr);
+}
