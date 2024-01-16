71b5571533e3f63b735ea73ac481e56eb6997d1a
bouzagui <badrbouzagui@gmail.com>
2024-01-16 01:46:12 +0000

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * free_grid - free the grid structure.
+ * @grid: the grid structure.
+ * @height: the height of the grid structure.
+*/
 void free_grid(int **grid, int height)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < height)
-    {
-        free(grid[i]);
-    }
-    free(grid);
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
 }
