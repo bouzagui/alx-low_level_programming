b5725c200e111bde510f20bfb177fc27a339935e
bouzagui <badrbouzagui@gmail.com>
2024-01-16 02:16:16 +0000

    done

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,48 @@
 #include "main.h"
 
+/**
+ * alloc_grid - allocate a grid from the given 2 dimensions.
+ * @width: the width of the grid.
+ * @height: the height of the grid.
+ * Return: the newly allocated grid.
+*/
 int **alloc_grid(int width, int height)
 {
-    int **grid, i = 0;
+	int **grid, i = 0, j = 0;
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
-    grid = (int**) malloc(width * sizeof(int*));
-    if (grid == NULL)
-    {
-        exit(0);
-    }
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = (int**) malloc(width * sizeof(int*));
+	if (grid == NULL)
+	{
+		exit(0);
+	}
 
-    while (i < width)
-    {
-        grid[i] = (int*)malloc(height * sizeof(int));
-        if (grid[i] == NULL)
-        {
-            while (i > 0)
-            {
-                free(grid[--i]);
-            }
-            free(grid);
-            return (NULL);
-        }
-        i++;
-    }
-    return (grid);
+	while (i < width)
+	{
+		grid[i] = (int*)malloc(height * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (j <= i)
+			{
+				free(grid[j]);
+				j++;
+			}
+			free(grid);
+			return (NULL);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+		i++;
+	}
+	return (grid);
 }
