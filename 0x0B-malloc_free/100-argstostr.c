0b399c7b07e39a17630a2aaab5ab2b606ceb0b0d
bouzagui <badrbouzagui@gmail.com>
2024-01-17 00:19:59 +0000

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,39 +1,47 @@
 #include "main.h"
 #include <string.h>
-#include <stdlib.h>
 
+/**
+ * argstostr - output string for argstost for command line parsing
+ * @ac: command line arguments
+ * @av: argument
+ * Return: output string.
+*/
 char *argstostr(int ac, char **av)
 {
-    int i = 0, j = 0;
-    char *ptr;
-    size_t len = 0;
+	int i = 0, j = 0;
+	char *ptr;
+	size_t len = 0;
 
-    if (ac == 0 || av == NULL)
-    {
-        exit(0);
-    }
-    while (i < ac)
-    {
-        len += strlen(av[i]);
-        i++;
-    }
-    ptr = malloc((len + ac) * sizeof(char));
-    if (ptr == NULL)
-    {
-        exit(0);
-    }
-    i = 0;
-    while (i < ac)
-    {
-        j = 0;
-        while (ptr[j] != '\0')
-        {
-            ptr[i + j] = av[i][j];
-            j++;
-            i++;
-        }
-        ptr[i] = '\n';
-    }
-    ptr[i] = '\0';
-    return (ptr);
+	if (ac == 0 || av == NULL)
+	{
+		exit(0);
+	}
+	while (i < ac)
+	{
+		len += strlen(av[i]);
+		i++;
+	}
+	ptr = malloc((len + ac) * sizeof(char));
+	if (ptr == NULL)
+	{
+		exit(0);
+	}
+	i = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (ptr[j] != '\0')
+		{
+			ptr[i + j] = av[i][j];
+			j++;
+		}
+		if (i < ac - 1)
+		{
+			ptr[i + j] = '\n';
+		}
+		i += j;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
