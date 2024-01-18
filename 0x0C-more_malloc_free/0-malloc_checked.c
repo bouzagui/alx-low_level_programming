dff47b09f1ebbfcfa7cab88e65d525e2c01b721c
bouzagui <badrbouzagui@gmail.com>
2024-01-18 13:56:09 +0000

    done

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - allocate a checked.
+ * @b: the buffer to be checked.
+ * Return: the buffer to be checked.
+*/
 void *malloc_checked(unsigned int b)
 {
-    void *ptr;
+	void *ptr;
 
-    ptr = malloc(b);
-    if (ptr == NULL)
-    {
-        exit(98);
-    }
-    return (ptr);
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
 }
