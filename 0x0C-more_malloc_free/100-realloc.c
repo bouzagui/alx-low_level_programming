078c1284d3b93d1856afc0df17ff3f87477589bf
bouzagui <badrbouzagui@gmail.com>
2024-01-19 01:59:34 +0000

    done

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Reallocates memory when new_size is greater than old_size
+ * @ptr: Pointer to the previously allocated memory
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: A pointer to the reallocated memory block, or NULL on failure.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *index, *p;
+	unsigned int a;
+
+	if (ptr == NULL)
+	{
+		index = malloc(new_size);
+		if (index == NULL)
+		{
+			return (NULL);
+		}
+		return (index);
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = ptr;
+	index = malloc(sizeof(*p) * new_size);
+
+	if (index == NULL)
+	{
+		exit(0);
+	}
+	a = 0;
+	while (a < old_size)
+	{
+		index[a] = p[a];
+		a++;
+	}
+
+	free(ptr);
+	return (index);
+}
+
+/**
+ * _realloc_expand - Reallocates memory when new_size is less than old_size
+ * @ptr: Pointer to the previously allocated memory
+ * @new_size: The size of the new memory block
+ *
+ * Return: A pointer to the reallocated memory block, or NULL on failure.
+ */
+void *_realloc_expand(void *ptr, unsigned int size)
+{
+	char *index, *p;
+	unsigned int b;
+
+	if (size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = ptr;
+	index = malloc(sizeof(*p) * size);
+
+	if (index == NULL)
+	{
+		exit(0);
+	}
+	b = 0;
+	while (b < size)
+	{
+		index[b] = p[b];
+		b++;
+	}
+
+	free(ptr);
+	return (index);
+}
