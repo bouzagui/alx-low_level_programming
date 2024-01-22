7d03fcce4acb235bfb031b504bb04e0f3e9db33d
bouzagui <badrbouzagui@gmail.com>
2024-01-22 22:57:44 +0000

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,10 @@ struct dog
 	char *owner;
 };
 
-typedef struct Dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct Dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
