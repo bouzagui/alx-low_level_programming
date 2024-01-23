5bbca2176c716d5da1ae8e844b1fc57cd1c6e9b2
bouzagui <badrbouzagui@gmail.com>
2024-01-23 00:56:19 +0000

    done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - free a dog object from memory.
+ * @d: the dog object to free.
+*/
+void free_dog(dog_t *d)
+{
+    if (d != NULL)
+    {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
