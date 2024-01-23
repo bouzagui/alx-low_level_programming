c669c52f273f4e729ac266168a50379955af0b8c
bouzagui <badrbouzagui@gmail.com>
2024-01-23 00:47:07 +0000

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,46 +1,53 @@
 #include "dog.h"
 
+/**
+ * new_dog - create a new instance of the dog.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ * return: the new dog.
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    unsigned int i, k;
+	dog_t *dog;
+	unsigned int i, k;
 
-    if (name == NULL || owner == NULL)
-    {
-        return (NULL);
-    }
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-    {
-        return (NULL);
-    }
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
 
-    dog->name = malloc(strlen(name) + 1);
-    if (dog->name == NULL)
-    {
-        free(dog);
-        exit(0);
-    }
+	dog->name = malloc(strlen(name) + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		exit(0);
+	}
 
-    for (i = 0; i < strlen(name); i++)
-    {
-        dog->name[i] = name[i];
-    }
-    dog->name[i] = '\0';
+	for (i = 0; i < strlen(name); i++)
+	{
+		dog->name[i] = name[i];
+	}
+	dog->name[i] = '\0';
 
-    dog->age = age;
+	dog->age = age;
 
-    dog->owner = malloc(strlen(owner) + 1);
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        exit(0);
-    }
-    for (k = 0; k < strlen(owner); k++)
-    {
-        dog->owner[k] = owner[k];
-    }
-    dog->owner[k] = '\0';
-    return (dog);
+	dog->owner = malloc(strlen(owner) + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		exit(0);
+	}
+	for (k = 0; k < strlen(owner); k++)
+	{
+		dog->owner[k] = owner[k];
+	}
+	dog->owner[k] = '\0';
+	return (dog);
 }
