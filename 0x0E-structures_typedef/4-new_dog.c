ccd05c84f1379a4aa3f56121f2ab35515a38849d
bouzagui <badrbouzagui@gmail.com>
2024-01-23 00:42:14 +0000

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
     }
     for (k = 0; k < strlen(owner); k++)
     {
-        dog->name[k] = owner[k];
+        dog->owner[k] = owner[k];
     }
     dog->owner[k] = '\0';
     return (dog);
