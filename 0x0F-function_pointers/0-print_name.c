bbaa923ed7d8ae5c9b0bbeaf52e14942ce15e858
bouzagui <badrbouzagui@gmail.com>
2024-01-24 16:07:05 +0000

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,17 @@
 #include "function_pointers.h"
-
+/**
+ * print_name - function name for printing
+ * @name: name of function.
+ * @f:
+*/
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL && f == NULL)
-    {
-        exit(1);
-    }
-    else
-    {
-        f(name);
-    }
+	if (name == NULL && f == NULL)
+	{
+		exit(0);
+	}
+	else
+	{
+		f(name);
+	}
 }
