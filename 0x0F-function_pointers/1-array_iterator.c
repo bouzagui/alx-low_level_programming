6de32a3a4b82ab874118843ce496c7c9734847d3
bouzagui <badrbouzagui@gmail.com>
2024-01-24 18:45:27 +0000

    done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i = 0;
+    size_t i;
 
     if (array != NULL || action != NULL)
     {
+        i = 0;
         while (i < size)
         {
             action(array[i]);
