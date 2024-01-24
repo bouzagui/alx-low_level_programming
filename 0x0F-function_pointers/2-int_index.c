9bb5fd919de5a5254a05618f804aa384d37525d0
bouzagui <badrbouzagui@gmail.com>
2024-01-24 23:46:32 +0000

    done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,12 +4,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
     int i = 0;
 
-    if (array == NULL || cmp == NULL || size <= 0)
+    if (array == NULL && cmp == NULL)
+    {
+        exit(-1);
+    }
+    if (size <= 0)
     {
         exit(-1);
     }
     i = 0;
-    while (i < (int)size)
+    while (i < (int)size && size > 0)
     {
         if (cmp(array[i]) != 0)
         {
