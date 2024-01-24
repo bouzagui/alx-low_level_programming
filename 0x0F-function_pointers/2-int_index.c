e6c5d9f4c17be34dad765f1f1e4ddc800ef887db
bouzagui <badrbouzagui@gmail.com>
2024-01-24 23:50:25 +0000

    done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,25 +1,32 @@
 #include "function_pointers.h"
-
+/**
+ * int_index - entry point
+ * @array: size of triangle
+ * @size: size of triangle
+ * @cmp: size of triangle
+ * Description: --
+ * Return: --
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i = 0;
+	int i = 0;
 
-    if (array == NULL && cmp == NULL)
-    {
-        return (-1);
-    }
-    if (size <= 0)
-    {
-        return (-1);
-    }
-    i = 0;
-    while (i < (int)size)
-    {
-        if (cmp(array[i]) != 0)
-        {
-            return (i);
-        }
-        i++;
-    }
-    return (-1);
+	if (array == NULL && cmp == NULL)
+	{
+		exit(-1);
+	}
+	if (size <= 0)
+	{
+		exit(-1);
+	}
+	i = 0;
+	while (i < (int)size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	exit(-1);
 }
