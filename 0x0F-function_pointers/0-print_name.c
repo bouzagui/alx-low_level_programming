91fe74349579b6fd60a6e3d4694f3be2b67c8383
bouzagui <badrbouzagui@gmail.com>
2024-01-24 15:41:49 +0000

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL && f == NULL)
+    {
+        exit(1);
+    }
+    else
+    {
+        f(name);
+    }
+}
