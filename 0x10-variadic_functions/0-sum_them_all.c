b296004a2830eff4a0d87e5c6ab05dab1cb8deb6
bouzagui <badrbouzagui@gmail.com>
2024-01-25 18:36:59 +0000

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum them all numbers from one to another using the sum function.
+ * @n: numbers to sum together.
+ * Return: sum of all numbers.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+	va_list ap;
+
+	va_start(ap, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		sum = sum + va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
