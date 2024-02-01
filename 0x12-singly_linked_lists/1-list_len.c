32a4ef1c35989b38327db97b5631b92449845ec9
bouzagui <badrbouzagui@gmail.com>
2024-02-01 19:56:15 +0000

    done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,14 +1,19 @@
 #include "lists.h"
 
+/**
+ * list_len - length of the list.
+ * @h: pointer to the list.
+ * Return: the length of the list.
+*/
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *ifr = h;
+	size_t count = 0;
+	const list_t *ifr = h;
 
-    while (ifr != NULL)
-    {
-        count++;
-        ifr = ifr->next;
-    }
-    return (count);
+	while (ifr != NULL)
+	{
+		count++;
+		ifr = ifr->next;
+	}
+	return (count);
 }
