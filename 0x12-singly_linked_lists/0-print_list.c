4b28b026758ebe1af8b6de30b552e893ca3a0515
bouzagui <badrbouzagui@gmail.com>
2024-02-01 19:17:12 +0000

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,28 @@
 #include "lists.h"
 
+/**
+ * print_list - print a list of objects.
+ * @h: the header.
+ * Return: the number of objects.
+*/
 size_t print_list(const list_t *h)
 {
-    size_t count;
-    const list_t *corr = h;
+	size_t count;
+	const list_t *corr = h;
 
-    count = 0;
-    while (corr != NULL)
-    {
-        if (corr->str != NULL)
-        {
-            printf("[%i] %s\n", corr->len, corr->str);
-        }
-        else
-        {
-            printf("[0] (nil)\n");
-        }
-        count++;
-        corr = corr->next;
-    }
-    return (count);
+	count = 0;
+	while (corr != NULL)
+	{
+		if (corr->str != NULL)
+		{
+			printf("[%i] %s\n", corr->len, corr->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
+		corr = corr->next;
+	}
+	return (count);
 }
