524ea1e1fd0a7b792a950d6b9f72b86aa167606d
bouzagui <badrbouzagui@gmail.com>
2024-02-01 22:50:42 +0000

    done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,19 @@
 #include "lists.h"
 
+/**
+ * free_list - free a list from memory.
+ * @head: the head of the list.
+*/
 void free_list(list_t *head)
 {
-    list_t *tail = head;
-    list_t *count;
+	list_t *tail = head;
+	list_t *count;
 
-    while (tail != NULL)
-    {
-        count = tail->next;
-        free(tail->str);
-        free(tail);
-        tail = count;
-    }
+	while (tail != NULL)
+	{
+		count = tail->next;
+		free(tail->str);
+		free(tail);
+		tail = count;
+	}
 }
