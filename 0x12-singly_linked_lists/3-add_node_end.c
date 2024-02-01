e515a5228b91cc0ccb67cf01b5ec666c2583f4c1
bouzagui <badrbouzagui@gmail.com>
2024-02-01 21:23:41 +0000

    done

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *new_head;
+    list_t *last_head = *head;
+
+    new_head = malloc(sizeof(list_t));
+    if (new_head == NULL)
+    {
+        return (NULL);
+    }
+    new_head->str = strdup(str);
+    if (new_head->str == NULL)
+    {
+        free(new_head);
+        return (NULL);
+    }
+    new_head->len = strlen(str);
+    new_head->next = *head;
+    if (*head == NULL)
+    {
+        *head = new_head;
+        return (new_head);
+    }
+    while (last_head->next != NULL)
+    {
+        last_head = last_head->next;
+    }
+    last_head->next = new_head;
+    return (new_head);
+}
