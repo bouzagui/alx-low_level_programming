921bcadc11e8e770de42e59f2376a9435ed351f8
bouzagui <badrbouzagui@gmail.com>
2024-02-02 00:28:30 +0000

    done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,15 @@
 section .data
-    hello_school db 'Hello, Holberton', 0   ; Null-terminated string
-    newline db 10                           ; Newline character
-    format db '%s', 0                       ; Format specifier for printf
+	hello_school db 'Hello, Holberton', 10, 0
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    sub rsp, 8
+	sub rsp, 8
 
-    mov rdi, format
-    mov rsi, hello_school
-    call printf
+	mov rdi, hello_school
+	call printf
 
-    mov rdi, format
-    mov rsi, newline
-    call printf
-
-    add rsp, 8
-    ret
+	add rsp, 8
+	ret
