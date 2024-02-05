b015536479eb4bf28df9ff21f4ab4d93577e4fca
bouzagui <badrbouzagui@gmail.com>
2024-02-05 17:10:39 +0000

    done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,18 @@
 #include "lists.h"
-
+/**
+ * listint_len - instialisation element.
+ * @h: pointer to the list.
+ * Return: number of elements.
+*/
 size_t listint_len(const listint_t *h)
 {
-    const listint_t *list = h;
-    size_t count = 0;
+	const listint_t *list = h;
+	size_t count = 0;
 
-    while (list != NULL)
-    {
-        count++;
-        list = list->next;
-    }
-    return (count);
+	while (list != NULL)
+	{
+		count++;
+		list = list->next;
+	}
+	return (count);
 }
