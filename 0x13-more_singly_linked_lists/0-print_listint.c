3beca2f1acb51eaee2e9a41ea07f2c257c2b2f5e
bouzagui <badrbouzagui@gmail.com>
2024-02-05 16:21:22 +0000

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,20 @@
 #include "lists.h"
 
+/**
+ * print_listint - print a list of integers.
+ * @h: the header of the message.
+ * Return: the number of bytes.
+*/
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *p = h;
-    size_t count = 0;
+	const listint_t *p = h;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%i\n", p->n);
-        count++;
-        p = p->next;
-    }
-    return (count);
+	while (p != NULL)
+	{
+		printf("%i\n", p->n);
+		count++;
+		p = p->next;
+	}
+	return (count);
 }
