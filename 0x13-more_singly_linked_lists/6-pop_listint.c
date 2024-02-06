71a4debc175e37f58b17832047fd36cf6b331d82
bouzagui <badrbouzagui@gmail.com>
2024-02-06 01:25:49 +0000

    donw

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * pop_listint - pop a list.
+ * @head: the head of the list.
+ * Return: current position.
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *cur;
+	int count;
+
+	if (*head == NULL)
+	{
+		exit(1);
+	}
+	else
+	{
+		count = (*head)->n;
+		cur = *head;
+		*head = (*head)->next;
+		free(cur);
+	}
+	return (count);
+}
