d0326522f3fe2f676ed291186f68f952c42d050d
bouzagui <badrbouzagui@gmail.com>
2024-02-08 20:30:26 +0000

    done

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - flip bits from current.
+ * @n: the first number of bits to flip.
+ * @m: the second number of bits to flip.
+ * Return: the number of bits.
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+    unsigned long int shift;
+    unsigned long int count = 0;
+
+    shift = n ^ m;
+    while (shift != 0)
+    {
+        count += shift & 1;
+        shift >>= 1;
+    }
+    return (count);
+}
