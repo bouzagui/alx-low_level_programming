e50ccfe7f59ca844452c6f1547e47bd5cc51c8bd
bouzagui <badrbouzagui@gmail.com>
2024-02-08 18:30:03 +0000

    done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_but - get the current state of the function.
+ * @n: the number of arguments.
+ * @index: the index of the function.
+ * Return: the current state of the function.
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	if (index >= (sizeof(unsigned long int)) * 8)
+	{
+		return (-1);
+	}
+	bit = (n >> index) & 1;
+
+	return (bit);
+}
