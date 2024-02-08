e832f79366e1e14787d9f3a451e6e89d9f56e4de
bouzagui <badrbouzagui@gmail.com>
2024-02-08 19:02:41 +0000

    done

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * clear_bit - clear the bit field.
+ * @n: number of bits to clear.
+ * @index: index of the bit field to clear.
+ * Return: number of bits cleared.
+*/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int)) * 8)
@@ -8,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	}
 	else
 	{
-		*n = (1 << index) | *n;
+		*n = (~(1 << index) & *n);
 	}
 	return (1);
 }
