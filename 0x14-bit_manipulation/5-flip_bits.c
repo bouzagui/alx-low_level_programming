9be115ee4b51630d0cd2073884c7a31eb6ae3b24
bouzagui <badrbouzagui@gmail.com>
2024-02-08 20:31:37 +0000

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,14 +7,14 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int shift;
-    unsigned long int count = 0;
+	unsigned long int shift;
+	unsigned long int count = 0;
 
-    shift = n ^ m;
-    while (shift != 0)
-    {
-        count += shift & 1;
-        shift >>= 1;
-    }
-    return (count);
+	shift = n ^ m;
+	while (shift != 0)
+	{
+		count += shift & 1;
+		shift >>= 1;
+	}
+	return (count);
 }
