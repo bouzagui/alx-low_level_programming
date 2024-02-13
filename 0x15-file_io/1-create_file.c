3b18ab07b31f22d5359f35c175893fdbbddce3b2
bouzagui <badrbouzagui@gmail.com>
2024-02-13 02:33:58 +0000

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		{
     		len++;
 		}
-		write(fp, text_content, len);
+		written = write(fp, text_content, len);
 		if (written == -1)
 		{
 			close(fp);
