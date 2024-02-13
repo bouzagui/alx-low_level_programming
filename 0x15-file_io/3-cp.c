70c394a644c859f92d97a2056cd27d3a5fb1e600
bouzagui <badrbouzagui@gmail.com>
2024-02-13 11:10:15 +0000

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,9 +15,10 @@ void copy_file(const char *file_from, const char *file_to)
 
     fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1)
+    {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
         exit(98);
-
+    }
     fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
     {
@@ -25,17 +26,17 @@ void copy_file(const char *file_from, const char *file_to)
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
         exit(99);
     }
-
     while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
     {
         bytes_written = write(fd_to, buf, bytes_read);
         if (bytes_written == -1)
+        {
             close(fd_from);
             close(fd_to);
             dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
             exit(99);
+        }
     }
-
     if (bytes_read == -1)
     {
         close(fd_from);
@@ -43,13 +44,11 @@ void copy_file(const char *file_from, const char *file_to)
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
         exit(98);
     }
-
     if (close(fd_from) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
         exit(100);
     }
-
     if (close(fd_to) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
