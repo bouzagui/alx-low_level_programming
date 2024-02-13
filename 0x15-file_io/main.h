9d35eee27f51284e79fcbea02bc39c421f93c4a3
bouzagui <badrbouzagui@gmail.com>
2024-02-13 02:56:13 +0000

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(const char *message, const char *arg, int exit_code);
+void copy_file(const char *file_from, const char *file_to);
 
 #endif
