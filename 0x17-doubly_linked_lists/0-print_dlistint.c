882c70f5bdc2ffbad40b4f4d7a4beafbceca2477
bouzagui <badrbouzagui@gmail.com>
2024-03-21 18:04:42 +0000

    Done ✅

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t i = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
