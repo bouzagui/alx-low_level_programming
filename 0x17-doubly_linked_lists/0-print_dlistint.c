17f2a3b7f92bd4d2f3dddc203c67de35ac1e2b6e
bouzagui <badrbouzagui@gmail.com>
2024-03-21 22:03:53 +0000

    Done ✅

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,13 +6,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        i++;
-    }
-    return (i);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,14 +4,14 @@
  * @h: head of the list
  * Return: the number of nodes.
 */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        i++;
-    }
-    return (i);
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
