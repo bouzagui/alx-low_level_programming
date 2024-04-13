3883ab03dc53c6c5928fac665b7c1edb0ecb77c4
bouzagui <badrbouzagui@gmail.com>
2024-04-13 02:10:04 +0000

    Done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,19 +6,26 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ptr;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (size == 0)
 		return (NULL);
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	ptr = malloc(sizeof(hash_table_t));
+	if (ptr == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	}
 
-	return (ht);
+	ptr->size = size;
+	ptr->array = malloc(size * sizeof(hash_table_t *));
+	if (ptr->array == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		ptr->array[i] = NULL;
+		i++;
+	}
+	return (ptr);
 }
