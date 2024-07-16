de52b777de9d2631caea6dde384522fad5a1a2ca
bouzagui <badrbouzagui@gmail.com>
2024-07-16 13:14:51 +0100

    done ✅

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,10 +11,6 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (array == NULL || !value)
-	{
-		return (-1);
-	}
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
